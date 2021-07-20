23344e42e96e462da2f8112ed6bf562973400104
mohamedclassics2 <mohamedclassics@gmail.com>
2021-07-21 00:16:41 +0100

    Mohamed3

---
+++ src/test/java/TBA/stepDefinitions/AvailableSeats.java
@@ -0,0 +1,39 @@
+package TBA.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AvailableSeats {
+
+    @Given("I am on SkyScanner login page")
+    public void i_am_on_SkyScanner_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I search for direct flights from London  to New York with {int} , Economy, {int} August {int} and British Airways")
+    public void i_search_for_direct_flights_from_London_to_New_York_with_Economy_August_and_British_Airways(Integer int1, Integer int2, Integer int3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Window seat is available message should appear")
+    public void window_seat_is_available_message_should_appear() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I search for direct flights from Paris  to New York with {int} , Business, {int} August {int} and Air France")
+    public void i_search_for_direct_flights_from_Paris_to_New_York_with_Business_August_and_Air_France(Integer int1, Integer int2, Integer int3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I search for direct flights from Berlin  to New York with {int} , Economy, {int} August {int} and Lufthansa")
+    public void i_search_for_direct_flights_from_Berlin_to_New_York_with_Economy_August_and_Lufthansa(Integer int1, Integer int2, Integer int3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
